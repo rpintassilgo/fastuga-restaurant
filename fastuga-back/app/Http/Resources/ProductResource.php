77b3d0e99095ec979e4015e1319cb8080e5bcd24
rpintassilgo <rodrigofpintassilgo@gmail.com>
2022-11-28 02:51:52 +0000

    fix rota get('product/{id}')

--- fastuga-back/app/Http/Resources/ProductResource.php
+++ fastuga-back/app/Http/Resources/ProductResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Product extends JsonResource
+class ProductResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
