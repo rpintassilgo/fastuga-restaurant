f6a9e83a0418b2e893abaffba65ad40e6f381ddf
Diogo Pereira <2201719@my.ipleiria.pt>
2022-12-22 15:04:42 +0000

    composition api update in statistic_order

--- fastuga-front/src/components/statistics/statistic_order.vue
+++ fastuga-front/src/components/statistics/statistic_order.vue
@@ -1,78 +1,88 @@
-<script>
+<script setup>
 import Chart from "chart.js/auto";
-import { ref } from "vue";
-import axios from "axios";
+import { ref, watch, computed, onMounted, inject } from "vue";
 
-const statistics = ref([]);
+const axios = inject("axios");
+
+const statistics = [];
 const productbyType = [];
-const serverBaseUrl = import.meta.env.VITE_APP_BASE_URL;
+const typeProducts = ["hot dish", "cold dish", "drink", "dessert"];
 
-async function vartypes(type) {
+const props = defineProps({
+  msg: String,
+});
+
+const vartypes = async (type) => {
   try {
-    const resp = await axios.get(serverBaseUrl + "/api/statistics/type/" + type, {
+    const resp = await axios.get(`statistics/type/${type}`, {
       headers: {
-        "Content-Type": " ",
+        "Content-Type": "application/json",
         Authorization: "Bearer " + sessionStorage.getItem("token"),
       },
     });
     statistics.value = resp.data;
-    console.log(JSON.stringify(statistics.value));
+
+    // console.log(statistics.value);
 
     return JSON.parse(JSON.stringify(statistics.value));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-const typeProducts = ["hot dish", "cold dish", "drink", "dessert"];
-for (let index = 0; index < typeProducts.length; index++) {
-  const countProducts = await vartypes(typeProducts[index]);
-  for (let x = 0; x < countProducts.length; x++) {
-    productbyType.push(countProducts[0]);
-    countProducts.shift();
+const graph = async () => {
+  for (let index = 0; index < typeProducts.length; index++) {
+    // console.log(typeProducts);
+    const countProducts = await vartypes(typeProducts[index]);
+    // console.log(countProducts);
+
+    // console.log(index);
+
+    // console.log(countProducts[2]);
+
+    for (let x = 0; x < countProducts.length; x++) {
+      productbyType.push(countProducts[x]);
+      // countProducts.shift();
+      // console.log(productbyType);
+    }
   }
+};
 
-}
+onMounted(async () => {
+  // console.log("component mounted");
+  // console.log(productbyType);
 
-export default {
-  name: "Hello",
-  props: {
-    msg: String,
-  },
-  mounted() {
-    console.log("component mounted");
+  const ctx = document.getElementById("myChart"); // node
 
-    const ctx = document.getElementById("myChart"); // node
+  await graph();
 
-    const myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Hot Dishes", "Cold Dishes", "Drinks", "Desserts"],
-        datasets: [
-          {
-            label: "# of Produt",
-            data: [
-              productbyType[0].count,
-              productbyType[1].count,
-              productbyType[2].count,
-              productbyType[3].count,
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Hot Dishes", "Cold Dishes", "Drinks", "Desserts"],
+      datasets: [
+        {
+          label: "# of Produt",
+          data: [
+            productbyType[0].count,
+            productbyType[1].count,
+            productbyType[2].count,
+            productbyType[3].count,
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
       },
-    });
-
-    myChart;
-  },
-};
+    },
+  });
+  myChart;
+});
 </script>
 
 <template>
@@ -98,11 +108,9 @@ export default {
 }
 .hello {
   margin: auto;
-  width: 45%;
+  width: 40%;
   margin-top: 1%;
   border: 3px solid black;
   padding: 10px;
 }
 </style>
-
-   
